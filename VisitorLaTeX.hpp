f8faf90cecd1e3bbaa68190875491e46a9955c19
alice-thaitea <athai026@ucr.edu>
2021-05-24 14:29:02 -0700

    added virtual sub class visit functions

--- VisitorLaTeX.hpp
+++ VisitorLaTeX.hpp
@@ -2,9 +2,19 @@
 #define __VISITOR_LATEX_HPP__
 
 #include "visitor.hpp"
+#include <iostream>
 
 class VisitorLaTeX : public Visitor {
-
+    public:
+        void visit_sub_begin(Sub* node) {
+            std::cout << "{(";
+        }
+        void visit_sub_middle(Sub* node) {
+            std::cout << "-";
+        }
+        void visit_sub_end(Sub* node) {
+            std::cout << ")}";
+        }
 };
 
 #endif //__VISITOR_LATEX_HPP__
