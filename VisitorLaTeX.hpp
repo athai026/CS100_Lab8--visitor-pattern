88200c6fb0152ac09f652ea74e523332cad9e973
alice-thaitea <athai026@ucr.edu>
2021-05-25 01:12:23 -0700

    Implemented visit function

--- VisitorLaTeX.hpp
+++ VisitorLaTeX.hpp
@@ -2,9 +2,25 @@
 #define __VISITOR_LATEX_HPP__
 
 #include "visitor.hpp"
+#include "iterator.hpp"
+#include "rand.hpp"
 
 class VisitorLaTeX : public Visitor {
+    public:
+        void visit_rand(Rand* node) {
+            output << "{";
+            output << node->stringify();
+            output << "}";
+        }
 
+        std::string PrintLaTeX(Base* ptr) {
+            output << "$";
+            for(Iterator it(ptr); !it.is_done(); it.next()) {
+                it.current_node()->accept(this, it.current_index());
+            }
+            output << "$";
+            return output.str();
+        }
 };
 
 #endif //__VISITOR_LATEX_HPP__
