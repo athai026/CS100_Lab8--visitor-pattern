aa3cf0600b15dffcc13f1b7291669b4bf4bfe664
alice-thaitea <athai026@ucr.edu>
2021-05-25 13:06:26 -0700

    Added (1-(2/3)) test

---
+++ mixedTest.hpp
@@ -0,0 +1,25 @@
+#ifndef __MIXEDTEST_HPP__
+#define __MIXEDTEST_HPP__
+
+#include <string>
+#include "gtest/gtest.h"
+#include "VisitorLaTeX.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "pow.hpp"
+#include "op.hpp"
+
+TEST(MixedTest, SubDiv) {
+    Base* one = new Op(1);
+    Base* two = new Op(2);
+    Base* three = new Op(3);
+    Base* div = new Div(two, three);
+    Base* sub = new Sub(one, div);
+    std::string result = "${({1}-{\\frac{2}{3}})}$";
+    VisitorLaTeX v;
+    EXPECT_EQ(v.PrintLaTeX(sub), result);
+}
+
+#endif //__MIXEDTEST_HPP__
