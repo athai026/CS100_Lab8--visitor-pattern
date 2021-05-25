e90ba9ff8bca0540ff92317bc7fb26c5a9c2a9eb
alice-thaitea <athai026@ucr.edu>
2021-05-24 23:07:15 -0700

    Edited get_child() and number_of_children() functions

--- sub.hpp
+++ sub.hpp
@@ -5,11 +5,15 @@
 
 class Sub : public Base {
     public: 
+        Base* l_child;
+        Base* r_child;
         Sub(Base* left, Base* right) : Base() {
             l_value = left->evaluate();
             r_value = right->evaluate();
             l_string = left->stringify();
             r_string = right->stringify();
+            l_child = left;
+            r_child = right;
         }
         virtual double evaluate() {
             difference = l_value - r_value;
@@ -19,19 +23,24 @@ class Sub : public Base {
             return ("(" + l_string + "-" + r_string + ")");
         }
         virtual int number_of_children() {
-            if ( left != nullptr && right != nullptr) {
-                return 2;
+            int num_child = 0;
+            if (l_child != nullptr) {
+                ++num_child;
             }
-            else {
-                return 0;
+            if (r_child != nullptr) {
+                ++num_child;
             }
+            return num_child;
         }
         virtual Base* get_child(int i) {
-            if (i  = 0) {
-                return left;
+            if (i  == 0) {
+                return l_child;
+            }
+            else if (i == 1) {
+                return r_child;
             }
             else {
-                return right;
+                return nullptr;
             }
         }
 
@@ -44,6 +53,9 @@ class Sub : public Base {
             }
             else if (index == 2) {
                 visitor->visit_sub_end(this);
+            }
+            else {
+                std::cout << "Error invalid index " << index << std::endl;
             }
         }
 
