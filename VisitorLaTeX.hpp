f8864b60cef8b2f117160f9d350928549c761cbc
alice-thaitea <athai026@ucr.edu>
2021-05-25 00:33:32 -0700

    Implemented visit functions

--- VisitorLaTeX.hpp
+++ VisitorLaTeX.hpp
@@ -2,9 +2,36 @@
 #define __VISITOR_LATEX_HPP__
 
 #include "visitor.hpp"
+#include "iterator.hpp"
+#include "op.hpp"
+#include "div.hpp"
 
 class VisitorLaTeX : public Visitor {
+    public:
+        void visit_op(Op* node) {
+            output << "{";
+            output << node->stringify();
+            output << "}";
+        }
 
+        void visit_div_begin(Div* node) {
+            output << "{\\frac";
+        }
+        void visit_div_middle(Div* node) {
+            output << "";
+        }
+        void visit_div_end(Div* node) {
+            output << "}";
+        }
+
+        std::string PrintLaTeX(Base* ptr) {
+            output << "$";
+            for(Iterator it(ptr); !it.is_done(); it.next()) {
+                it.current_node()->accept(this, it.current_index());
+            }
+            output << "$";
+            return output.str();
+        }
 };
 
 #endif //__VISITOR_LATEX_HPP__
